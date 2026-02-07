5637091a50ae40c4ae68aad4b2e0265f38b4a05d
Dipika Verma <dipikarakesh.verma@pfizer.com>
2026-02-07 20:12:16 +0530

    Update service_check.py

--- scripts/service_check.py
+++ scripts/service_check.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import subprocess
+import subprocess  #used to run external commands (here, ps -ef)
 import sys
 from read_config import get_env_config
 
@@ -9,7 +9,7 @@ def main():
     cfg = get_env_config()
     process_name = str(cfg.get("service_name", "python"))
 
-    result = subprocess.run(["ps", "-ef"], capture_output=True, text=True)
+    result = subprocess.run(["ps", "-ef"], capture_output=True, text=True)   #executes the Unix command ps -ef, which prints all running processes with details
     if process_name in result.stdout:
         print(f"✅ Process '{process_name}' is running")
     else:
